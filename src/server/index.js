bc05dd18f3bc9be342d79c3a9b0ab6af6f0a6a89
Christoffer Artmann <Artgaard@gmail.com>
2018-06-04 00:57:06 +0200

    Add endpoint that returns a list of suggestions for a specific number

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
 
+const SuggestionService = require ('./services/Suggestion');
+const suggestionService = new SuggestionService();
+
+app.get('/api/suggestions/:numbers', (request, response) => {
+  const numbers = `${request.params.numbers}`;
+  response.send(suggestionService.suggest(numbers));
+});
+
 app.use(express.static(`${__dirname}/../../public/`));
+
 app.listen(3000);
